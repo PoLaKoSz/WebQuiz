1d3541405f08f1730a00f289edd93c1a1d76b1de
PoLáKoSz Tom <PoLaKoSz@users.noreply.github.com>
2017-12-11 21:34:07 +0100

    Few Makroökonómia Q&A added

--- js/questions.js
+++ js/questions.js
@@ -2,39 +2,473 @@
 	"questions": [
 		{
 			"answers": [
-				"Válasz 1",
-				"Válasz 2",
+				"Összevont mutatók (pl. nemzeti jövedelem, foglalkoztatottság, infláció) segítségével a  gazdaság egészének teljesítményét, állapotát, fejlődési tendenciáit elemzi.",
+				"Az egymástól elkülönült piaci szereplők (emberek, háztartások, vállalatok) racionálisnak  feltételezett döntéseit vizsgálja.",
 			],
 			"correctIndexes": [
-				1,
+				0,
 			],
-			"question": "Kérdés 1?",
+			"question": "Mivel foglalkozik a Makroökonómia?",
 		},
 		{
 			"answers": [
-				"Válasz 1",
-				"Válasz 2",
-				"Válasz 3",
+				"Összevont mutatók (pl. nemzeti jövedelem, foglalkoztatottság, infláció) segítségével a  gazdaság egészének teljesítményét, állapotát, fejlődési tendenciáit elemzi.",
+				"Az egymástól elkülönült piaci szereplők (emberek, háztartások, vállalatok) racionálisnak  feltételezett döntéseit vizsgálja.",
 			],
 			"correctIndexes": [
 				1,
+			],
+			"question": "Mivel foglalkozik a Mikroökonómia?",
+		},
+		{
+			"answers": [
+				"A",
+				"L",
+				"K",
+			],
+			"correctIndexes": [
+				1,
+			],
+			"question": "Mi a Munka jele?",
+		},
+		{
+			"answers": [
+				"L",
+				"A",
+				"K",
+			],
+			"correctIndexes": [
+				1,
+			],
+			"question": "Mi a Természeti tényező jele?",
+		},
+		{
+			"answers": [
+				"K",
+				"L",
+				"A",
+				"M",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi a Tőkejavak jele?",
+		},
+		{
+			"answers": [
+				"Q",
+				"I",
+				"E",
+				"Y",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi a Kibocsátás jele?",
+		},
+		{
+			"answers": [
+				"C",
+				"Q",
+				"W",
+				"A",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi a Fogyasztás jele?",
+		},
+		{
+			"answers": [
+				"S",
+				"W",
+				"A",
+				"K",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi a Megtakarítás jele?",
+		},
+		{
+			"answers": [
+				"A pénzintézetek a náluk elhelyezett megtakarítás után kamatot fizetnek.",
+				"A pénzintézetek a náluk elhelyezett megtakarítás után kamatot fizetnek, amiből már le van vonva az infláció értéke.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az a Nominális Kamat?",
+		},
+		{
+			"answers": [
+				"Árszínvonal növekedését.",
+				"Fogyasztói árindexet növekedését.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mit nevezünk inflációnak?",
+		},
+		{
+			"answers": [
+				"Árszínvonal csökkenése.",
+				"Fogyasztói árindexet csökkenése.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az a defláció?",
+		},
+		{
+			"answers": [
+				"Nominális kamat inflációs rátával csökkentett értéke %-ban.",
+				"Nominális kamat %-ban.",
+				"reálkamat [%] = (nominális kamat) - (inflációs ráta)",
+			],
+			"correctIndexes": [
+				0,
 				2,
 			],
-			"question": "Kérdés 2?",
+			"question": "Mi az a reálkamat?",
 		},
 		{
 			"answers": [
-				"Válasz 1",
-				"Válasz 2",
-				"Válasz 3",
-				"Válasz 4",
+				"reálkamat = kamat - infláció = 6% - 4% =  2%",
+				"reálkamat = kamat + infláció = 6% + 4% = 10%",
+				"reálkamat = kamat * infláció = 6% * 4% = 24%",
+				"reálkamat = kamat / infláció = 6% / 4% = 1.5%",
 			],
 			"correctIndexes": [
+				0,
+			],
+			"question": "Mennyi lesz a Reálkamat értéke? 6%-os kamat és 4%-os infláció.",
+		},
+		{
+			"answers": [
+				"Tartozik számlakar (= kiadás)",
+				"Követel számlakar (= bevétel)",
+				"Akasztófa? :O",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogy nevezzük az akasztófa / számlakar bal oldalát?",
+		},
+		{
+			"answers": [
+				"Követel számlakar (= bevétel)",
+				"Tartozik számlakar (= kiadás)",
+				"Akasztófa? :O",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogy nevezzük az akasztófa / számlakar jobb oldalát?",
+		},
+		{
+			"answers": [
+				"A folyótétel számlákon a kiadások és bevételek összege egyenlő.",
+				"A számlakorok sosem kiegyenlítettek.",
+				"Makrógazdasági szereplők számláin mindig a kiadás a nagyobb.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mely állítás(ok) helyes(ek)?",
+		},
+		{
+			"answers": [
+				"Háztartási szektorhoz.",
+				"Állami szektorhoz.",
+				"Vállalati szektorhoz.",
+				"Tőkepiaci szektorhoz.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mely szektor(ok)hoz köthető a Fogyasztás (C)?",
+		},
+		{
+			"answers": [
+				"Tőkepiaci szektorhoz.",
+				"Vállalati szektorhoz.",
+				"Állami szektorhoz.",
+				"Háztartási szektorhoz.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mely szektor(ok)hoz köthető a Beruházás (I)?",
+		},
+		{
+			"answers": [
+				"Háztartási szektorhoz.",
+				"Vállalati szektorhoz.",
+				"Tőkepiaci szektorhoz.",
+				"Állami szektorhoz.",
+			],
+			"correctIndexes": [
+				0,
 				1,
 				2,
 				3,
 			],
-			"question": "Kérdés 3?",
-		}
+			"question": "Mely szektor(ok)hoz köthető a Megtakarítás (S)?",
+		},
+		{
+			"answers": [
+				"Vállalati szektorhoz.",
+				"Háztartási szektorhoz.",
+				"Tőkepiaci szektorhoz.",
+				"Állami szektorhoz.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mely szektor(ok)hoz köthető a Jövedelem (Y)?",
+		},
+		{
+			"answers": [
+				"Vállalati szektorhoz.",
+				"Háztartási szektorhoz.",
+				"Tőkepiaci szektorhoz.",
+				"Állami szektorhoz.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mely szektor(ok)hoz köthető a Transzfer (TR)?",
+		},
+		{
+			"answers": [
+				"Vállalati szektorhoz.",
+				"Háztartási szektorhoz.",
+				"Tőkepiaci szektorhoz.",
+				"Állami szektorhoz.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mely szektor(ok)hoz köthető a Bér (w)?",
+		},
+		{
+			"answers": [
+				"Vállalati szektorhoz.",
+				"Háztartási szektorhoz.",
+				"Állami szektorhoz.",
+				"Tőkepiaci szektorhoz.",
+			],
+			"correctIndexes": [
+				0,
+				1,
+				2,
+			],
+			"question": "Mely szektor(ok)hoz köthető az Adó (T)?",
+		},
+		{
+			"answers": [
+				"G",
+				"C",
+				"O",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi a Kormányzati kiadások jele?",
+		},
+		{
+			"answers": [
+				"Államnál.",
+				"Háztartásnál.",
+				"Vállalatoknál.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Kinél beszélhetünk Szufficitről?",
+		},
+		{
+			"answers": [
+				"Az állam számláján a pozitív (+) egyenleg.",
+				"Az állam számláján a negatív (-) egyenleg.",
+				"A vállalat számláján a pozitív (+) egyenleg.",
+				"A vállalat számláján a negatív (-) egyenleg.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az a Szufficit?",
+		},
+		{
+			"answers": [
+				"Államnál.",
+				"Háztartásnál.",
+				"Vállalatoknál.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Kinél beszélhetünk Deficitről?",
+		},
+		{
+			"answers": [
+				"Az állam számláján a negatív (-) egyenleg.",
+				"Az állam számláján a pozitív (+) egyenleg.",
+				"A vállalat számláján a pozitív (+) egyenleg.",
+				"A vállalat számláján a negatív (-) egyenleg.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az a Deficit?",
+		},
+		{
+			"answers": [
+				"Állami szektor.",
+				"Háztartási szektor.",
+				"Vállalati szektor.",
+				"Tőkepiaci szektor.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mely szektor szokott jellemzően negatív egyenleggel rendelkezni?",
+		},
+		{
+			"answers": [
+				"2004. május 1.",
+				"2003. május 1.",
+				"2005. május 1.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mióta tagja Magyarország az EU-nak?",
+		},
+		{
+			"answers": [
+				"X",
+				"E",
+				"EX",
+				"Ex",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az Export jele?",
+		},
+		{
+			"answers": [
+				"IM",
+				"Im",
+				"I",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az Import jele?",
+		},
+		{
+			"answers": [
+				"Jobb oldalra.",
+				"Bal oldalra.",
+				"Nem tartozik Külföldhöz Im.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Melyik oldalra kell írni az akasztófán belül az IM-et Külföldnél?",
+		},
+		{
+			"answers": [
+				"Bal oldalra.",
+				"Jobb oldalra.",
+				"Nem tartozik Külföldhöz X.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Melyik oldalra kell írni az akasztófán belül az X-et Külföldnél?",
+		},
+		{
+			"answers": [
+				"Nálunk beáramló jövedelem.",
+				"Nálunk kiáramló jövedelem.",
+				"Egyik sem.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az Export (X)?",
+		},
+		{
+			"answers": [
+				"Nálunk kiáramló jövedelem.",
+				"Nálunk beáramló jövedelem.",
+				"Egyik sem.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az Import (Im)?",
+		},
+		{
+			"answers": [
+				"Megtakarítás.",
+				"Hitel.",
+				"Semmi.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Külföldnél mi hozza egyensúlyba az Import (Im) és Export (X) különbséget?",
+		},
+		{
+			"answers": [
+				"NX = (X - IM)",
+				"NX = (X + IM)",
+				"NX = (X * IM)",
+				"NX = (X / IM)",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az a Nettó Export (NX)?",
+		},
+		{
+			"answers": [
+				"Háztartások.",
+				"Vállalatok.",
+				"Külföld.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mely szektor(ok) állítják elő az Export (X) termékeket?",
+		},
+		{
+			"answers": [
+				"Az állítás Igaz.",
+				"Az állítás Hamis.",
+				"Nem dönthető el az alábbi szövegből.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Az Import (I) csökkenti a (hazai) Vállalat termékei iránti keresletet.",
+		},
+		{
+			"answers": [
+				"Y = C + I + G + (X - IM)",
+				"Y = C + I + G + NX",
+				"Y = C + I + G",
+			],
+			"correctIndexes": [
+				0,
+				1,
+			],
+			"question": "Mivel egyenlő a Hazai Össztermék?",
+		},
 	]
 }
