07b0921e8b122f18af1db9760be0c16843fbf4c4
PoLáKoSz Tom <PoLaKoSz@users.noreply.github.com>
2017-12-12 16:58:02 +0100

    Added more Q&A

--- js/questions.js
+++ js/questions.js
@@ -22,40 +22,6 @@
 		},
 		{
 			"answers": [
-				"A",
-				"L",
-				"K",
-			],
-			"correctIndexes": [
-				1,
-			],
-			"question": "Mi a Munka jele?",
-		},
-		{
-			"answers": [
-				"L",
-				"A",
-				"K",
-			],
-			"correctIndexes": [
-				1,
-			],
-			"question": "Mi a Természeti tényező jele?",
-		},
-		{
-			"answers": [
-				"K",
-				"L",
-				"A",
-				"M",
-			],
-			"correctIndexes": [
-				0,
-			],
-			"question": "Mi a Tőkejavak jele?",
-		},
-		{
-			"answers": [
 				"Q",
 				"I",
 				"E",
@@ -169,7 +135,7 @@
 		{
 			"answers": [
 				"A folyótétel számlákon a kiadások és bevételek összege egyenlő.",
-				"A számlakorok sosem kiegyenlítettek.",
+				"A számlakarok sosem kiegyenlítettek.",
 				"Makrógazdasági szereplők számláin mindig a kiadás a nagyobb.",
 			],
 			"correctIndexes": [
@@ -360,7 +326,6 @@
 		},
 		{
 			"answers": [
-				"IM",
 				"Im",
 				"I",
 			],
@@ -378,7 +343,7 @@
 			"correctIndexes": [
 				0,
 			],
-			"question": "Melyik oldalra kell írni az akasztófán belül az IM-et Külföldnél?",
+			"question": "Melyik oldalra kell írni az akasztófán belül az Im-et Külföldnél?",
 		},
 		{
 			"answers": [
@@ -469,6 +434,556 @@
 				1,
 			],
 			"question": "Mivel egyenlő a Hazai Össztermék?",
+		},
+		{
+			"answers": [
+				"Szürkegazdaság.",
+				"Feketegazdaság.",
+			],
+			"correctIndexes": [
+				0,
+				1,
+			],
+			"question": "Mely gazdasági forma tekinthető illegálisnak?",
+		},
+		{
+			"answers": [
+				"Anyuka levágja a fiúgyermek haját.",
+				"Apuka kifesti a gyerekszobát.",
+				"Sógorral disznót vágunk.",
+				"Öcsi bevásárol Nagyinak.",
+			],
+			"correctIndexes": [
+
+			],
+			"question": "Mely tevékenységek számítanak be a GDP-be?",
+		},
+		{
+			"answers": [
+				"Igen, a helyreállítás, ujjáépítés miatt.",
+				"Nem.",
+				"Nem, mert a jólét csökkenésével jár.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Környezeti katasztrófa növeli-e a gazdaság teljesítményét?",
+		},
+		{
+			"answers": [
+				"Bruttó kibocsátás.",
+				"Nettó kibocsátás.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az Összes Kibocsátás (GO) másik neve?",
+		},
+		{
+			"answers": [
+				"GO",
+				"SQ",
+				"Q",
+				"X",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az Összes Kibocsátás jele?",
+		},
+		{
+			"answers": [
+				"GO = ∑ (p*Q)",
+				"Összes Kibocsátás = ∑ (Megtermelt mennyiség * Termék ára)",
+				"X - IM",
+				"IM - X",
+			],
+			"correctIndexes": [
+				0,
+				1,
+			],
+			"question": "Hogyan számoljuk ki az Össes Kibocsátás (GO)-t?",
+		},
+		{
+			"answers": [
+				"Háztartási szektor.",
+				"Vállalati szektor.",
+				"Magánszektor.",
+				"Egyik sem.",
+			],
+			"correctIndexes": [
+				0,
+				1,
+				2,
+			],
+			"question": "Mely szektor(ok) teljesítménye számít bele az Összes Kibocsátás (GO)-ba?",
+		},
+		{
+			"answers": [
+				"MaSzek",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogyan rövidíthetjük a Magánszektor kifejezést?",
+		},
+		{
+			"answers": [
+				"Adott év Kibocsátása adott év árain.",
+				"Két év Kibocsátásának összehasonlításakor a második év Kibocsátása.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az a Nominális Kibocsátás?",
+		},
+		{
+			"answers": [
+				"Igen.",
+				"Nem.",
+				"A szöveg alapján nem eldönthető.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Lehet-e két egymást követő év Kibocsátását is Nominális Kibocsátásnak nevezni?",
+		},
+		{
+			"answers": [
+				"Igen.",
+				"Nem.",
+				"A szöveg alapján nem eldönthető.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Két egymást követő évben ha a Termék mennyisége (p) és a Termék ára is nőtt, akkor a Nominális Kibocsátás is nőtt.",
+		},
+		{
+			"answers": [
+				"Folyó áras mutatónak.",
+				"Rögzített áras mutatónak.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogyan hívják más néven a Nominális mutatót (= Nominális Kibocsátást)?",
+		},
+		{
+			"answers": [
+				"Rögzített áras (bázisáras) mutatónak.",
+				"Folyó áras mutatónak.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogyan hívják más néven a Reálmutatót (= Reál Kibocsátást)?",
+		},
+		{
+			"answers": [
+				"A Nominális mindig az adott év összes termékének árát szorozza a termék mennyiségével, és ezek összegét hasonlítja hozzá a másik évben számolt összes megtermelt termék árának és mennyiségének szorzatával.",
+				"A Reál Kibocsátás úgy működik, hogy egy bázis (= alapként kijelölt) év árán számolja egy másik év ugyanazon termék mennyiségét, majd a bázis illetve az eredményül kapott összeget hasonlítja össze.",
+				"Az egyik csak 1 évet, míg a másik 2 évet vizsgál.",
+			],
+			"correctIndexes": [
+				0,
+				1,
+			],
+			"question": "Mi a különbség a Nominális (folyó áras) és a Reál (bázisáras) mutató között?",
+		},
+		{
+			"answers": [
+				"p",
+				"C",
+				"m",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Mi az Ár jele?",
+		},
+		{
+			"answers": [
+				"GDP = Folyó áras mutató / Bázisáras mutató",
+				"GDP = Nominális Kibocsátás / ReálKibocsátás",
+				"GDP = Folyó áras mutató / Bázisáras mutató * 100",
+				"GDP = Folyó áras mutató / (Bázisáras mutató * 100)",
+			],
+			"correctIndexes": [
+				0,
+				1,
+			],
+			"question": "Hogyan számoljuk a GDP árindexet?",
+		},
+		{
+			"answers": [
+				"ReálKibocsátás = Nominális Kibocsátás / GDP árindex",
+				"ReálKibocsátás = GDP árindex / Nominális Kibocsátás",
+				"Ezek közül egyikkel sem.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogyan számolható ki a ReálKibocsátás?",
+		},
+		{
+			"answers": [
+				"Ezek közül egyikkel sem.",
+				"ReálKibocsátás = Nominális Kibocsátás * GDP árindex",
+				"ReálKibocsátás = GDP árindex / Nominális Kibocsátás",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogyan számolható ki a ReálKibocsátás?",
+		},
+		{
+			"answers": [
+				"A Hozzáadott érték 5 millió - 1 millió = 4 millió.",
+				"Hozzáadott érték = Összes kibocsátás - Folyó termelő felhasználás",
+				"Halmozódás van az alábbi feladatban.",
+				"A Kibocsátás (Q) 1 millió + 5 millió = 6 millió.",
+			],
+			"correctIndexes": [
+				0,
+				1,
+				2,
+			],
+			"question": "Vackor Bt. vadon termő erdei gyümölcsök begyűjtésével foglalkozik, csak munkát alkalmaz a termelésben. A gyümölcsöket eladja 1 millió forintért a Pisze Pálinkafőző Kft-nek. A pálinkafőző pálinkát készít, majd azt eladja 5 millió forintért. Melyik állítás igaz?",
+		},
+		{
+			"answers": [
+				"Mikor egy Vállalat vesz valamit, amiből készít(het) valami más terméket és azt továbbadja drágábban és a számolásba belevesszük az eladó nyereségét is.",
+				"Hozzáadott érték számolásakor.",
+				"Reál Kibocsátás számolásakor.",
+			],
+			"correctIndexes": [
+				0,
+				1,
+
+			],
+			"question": "Mikor beszélünk Halmozódásról?",
+		},
+		{
+			"answers": [
+				"Gross Domestic Product",
+				"Global Domestic Price",
+				"General Domestic Product",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Minek a rövidítése a GDP?",
+		},
+		{
+			"answers": [
+				"GDP = Összes Kibocsátás - Folyó termelő felhasználás.",
+				"GDP = Összes Kibocsátás + Folyó termelő felhasználás.",
+				"GDP = Összes Kibocsátás * Folyó termelő felhasználás.",
+				"GDP = Összes Kibocsátás / Folyó termelő felhasználás.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogyan számoljuk ki a GDP értékét?",
+		},
+		{
+			"answers": [
+				"GDP",
+				"GNI",
+				"NDP",
+				"NNI",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogy hívjuk a Bruttó Hazai Terméket más néven?",
+		},
+		{
+			"answers": [
+				"Egy nagyobb beruházás következtében a nyereséget nem lehet az első évben mind elszámolni.",
+				"NDP számolásakor",
+				"Vállalati szektorban a gépek tönkremenetelekor.",
+				"GDP számolásakor",
+			],
+			"correctIndexes": [
+				0,
+				1,
+			],
+			"question": "Mikor beszélünk Amortizációról?",
+		},
+		{
+			"answers": [
+				"Net Domestic Product",
+				"Net Domestic Price",
+				"National Domestic Product",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Minek a rövidítése az NDP?",
+		},
+		{
+			"answers": [
+				"NDP",
+				"GDP",
+				"GNI",
+				"NNI",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogy hívjuk a Nettó Hazai Terméket más néven?",
+		},
+		{
+			"answers": [
+				"NDP = GDP - Amortizáció.",
+				"NDP = GDP + Amortizáció.",
+				"NDP = GDP * Amortizáció.",
+				"NDP = GDP / Amortizáció.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogyan számoljuk ki a NDP értékét?",
+		},
+		{
+			"answers": [
+				"Y = GDP",
+				"Y = NDP",
+				"Y = GNI",
+				"Y = NNI",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Ha a Beruházás (I)-okat bruttóként vesszük figyelembe a Hazai Össztermék számolásakor, akkor mely állítás(ok) helyes(ek)?",
+		},
+		{
+			"answers": [
+				"Y = NDP",
+				"Y = GDP - Amortizáció",
+				"Y = GDP",
+				"Y = GNI",
+			],
+			"correctIndexes": [
+				0,
+				1,
+			],
+			"question": "Ha a Beruházás (I)-okat nettóként vesszük figyelembe a Hazai Össztermék számolásakor, akkor mely állítás(ok) helyes(ek)?",
+		},
+		{
+			"answers": [
+				"Adott ország hazai és külföldi gazdasági szereplők teljesítménye egyaránt.",
+				"Csak az adott ország hazai gazdasági szereplők teljesítményét.",
+				"Adott ország állampolgárainak az adott országban és külföldön mért teljesítménye egyaránt.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "GDP számolásakor gazdasági szereplők tevékenységei kerülnek beleszámításba?",
+		},
+		{
+			"answers": [
+				"GDP = Hazaiak hazai termelése + Külföldiek itteni termelése",
+				"GDP = GNI - Hazaiak külföldi jövedelme + Külföldiek itteni termelése",
+				"GDP = Hazaiak hazai termelése - Külföldiek itteni termelése",
+				"GDP = GNI - Hazaiak külföldi jövedelme - Külföldiek itteni termelése",
+			],
+			"correctIndexes": [
+				0,
+				1,
+			],
+			"question": "Mi a GDP képlete?",
+		},
+		{
+			"answers": [
+				"Gross National Income",
+				"General Net Income",
+				"General National Income",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Minek a rövidítése az GNI?",
+		},
+		{
+			"answers": [
+				"Bruttó Nemzeti Jövedelem-nek.",
+				"Bruttó Hazai Jövedelem-nek.",
+				"Nettó Nemzeti Jövedelem-nek.",
+				"Nettó Hazai Jövedelem-nek.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Minek a rövidítése az GNI?",
+		},
+		{
+			"answers": [
+				"GNI",
+				"GDP",
+				"NDP",
+				"NNI",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogy hívjuk a Bruttó Nemzeti Jövedelmet más néven?",
+		},
+		{
+			"answers": [
+				"GNI = Hazaiak hazai jövedelme + Hazaiak külföldi jövedelme.",
+				"GNI = GDP - Külföldiek itteni termelése + Hazaiak külföldi jövedelme.",
+				"GNI = Hazaiak hazai jövedelme - Hazaiak külföldi jövedelme.",
+				"GNI = GDP - Külföldiek itteni termelése - Hazaiak külföldi jövedelme.",
+			],
+			"correctIndexes": [
+				0,
+				1,
+			],
+			"question": "Hogyan számoljuk ki a GNI értékét?",
+		},
+		{
+			"answers": [
+				"Net National Income",
+				"National Net Income",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Minek a rövidítése az NNI?",
+		},
+		{
+			"answers": [
+				"Nettó Nemzeti Jövedelem-nek.",
+				"Nettó Hazai Jövedelem-nek.",
+				"Bruttó Nemzeti Jövedelem-nek.",
+				"Bruttó Hazai Jövedelem-nek.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Minek a rövidítése az NNI?",
+		},
+		{
+			"answers": [
+				"NNI",
+				"GDP",
+				"NDP",
+				"GNI",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogy hívjuk a Nettó Nemzeti Jövedelmet más néven?",
+		},
+		{
+			"answers": [
+				"NNI = GNI – Amortizáció.",
+				"NNI = (GDP - (Külföldiek itteni termelése + Hazaiak külföldi jövedelme)) - Amortizáció.",
+				"NNI = GNI + Amortizáció.",
+				"NNI = (GDP + (Külföldiek itteni termelése + Hazaiak külföldi jövedelme)) + Amortizáció.",
+			],
+			"correctIndexes": [
+				0,
+				1,
+			],
+			"question": "Hogyan számoljuk ki az NNI értékét?",
+		},
+		{
+			"answers": [
+				"Gross National Disposable Income",
+				"Domestic Net Disposable Income",
+				"Domestic National Disposable Income",
+				"Gross Net Disposable Income",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Minek a rövidítése az GNDI?",
+		},
+		{
+			"answers": [
+				"Rendelkezésre álló Bruttó Nemzeti Jövedelem-nek.",
+				"Rendelkezésre álló Nettó Nemzeti Jövedelem-nek.",
+				"Rendelkezésre álló Bruttó Hazai Jövedelem-nek.",
+				"Rendelkezésre álló Nettó Hazai Jövedelem-nek.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Minek a rövidítése az GNDI?",
+		},
+		{
+			"answers": [
+				"GNDI",
+				"NNDI",
+				"NDP",
+				"GNI",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogy hívjuk a Rendelkezésre álló Bruttó Nemzeti Jövedelmet más néven?",
+		},
+		{
+			"answers": [
+				"GNDI = GNI - Kiáramló nemzetközi transzferek + Beáramló nemzetközi Transzferek",
+				"GNDI = (GNI - Kiáramló nemzetközi transzferek) + Beáramló nemzetközi Transzferek",
+				"GNDI = GNI - Kiáramló nemzetközi transzferek - Beáramló nemzetközi Transzferek",
+			],
+			"correctIndexes": [
+				0,
+				1,
+			],
+			"question": "Hogyan számoljuk ki az GNDI értékét?",
+		},
+		{
+			"answers": [
+				"Net National Disposable Income",
+				"National Net Disposable Income",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Minek a rövidítése az NNDI?",
+		},
+		{
+			"answers": [
+				"Rendelkezésre álló Nettó Nemzeti Jövedelem-nek.",
+				"Rendelkezésre álló Bruttó Nemzeti Jövedelem-nek.",
+				"Rendelkezésre álló Bruttó Hazai Jövedelem-nek.",
+				"Rendelkezésre álló Nettó Hazai Jövedelem-nek.",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Minek a rövidítése az NNDI?",
+		},
+		{
+			"answers": [
+				"NNDI",
+				"GNDI",
+				"NDP",
+				"GNI",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogy hívjuk a Rendelkezésre álló Nettó Nemzeti Jövedelmet más néven?",
+		},
+		{
+			"answers": [
+				"NNDI = GNDI – Amortizáció",
+				"NNDI = GNDI + Amortizáció",
+				"NNDI = GNDI * Amortizáció",
+				"NNDI = GNDI / Amortizáció",
+			],
+			"correctIndexes": [
+				0,
+			],
+			"question": "Hogyan számoljuk ki az GNDI értékét?",
 		},
 	]
 }
